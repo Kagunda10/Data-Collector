48e83c91623359b3eec0e8df48cd09433f0566cf
kagunda10 <caleb.njiiri@gmail.com>
2019-05-30 16:43:27 +0300

    Code refactoring of realo.py

--- realo.py
+++ realo.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 
 # get the link to cities
 # returns a list containing links to the cities
+
+
 def get_realo_city_links():
     main_page = 'https://www.realo.be/en'
     req = Request(main_page, headers={'User-Agent': 'Mozilla/5.0'})
@@ -12,16 +14,22 @@ def get_realo_city_links():
 
     city_houses_for_sale = []
     city_houses_for_rent = []
-    for each in main_page_soup.findAll('li', attrs={'data-id':'listContainer'}):
+    for each in main_page_soup.findAll(
+        'li', attrs={
+            'data-id': 'listContainer'}):
         if each.div.string.strip() == 'Houses for sale':
-            city_houses_for_sale.append(['https://www.realo.be{}'.format(x.a['href']) for x in each.ul.findAll('li')])
+            city_houses_for_sale.append(
+                ['https://www.realo.be{}'.format(x.a['href']) for x in each.ul.findAll('li')])
         elif each.div.string.strip() == 'Houses to rent':
-            city_houses_for_rent.append(['https://www.realo.be{}'.format(x.a['href']) for x in each.ul.findAll('li')])
+            city_houses_for_rent.append(
+                ['https://www.realo.be{}'.format(x.a['href']) for x in each.ul.findAll('li')])
 
-    return {'sale':city_houses_for_sale, 'rent':city_houses_for_rent}
+    return {'sale': city_houses_for_sale, 'rent': city_houses_for_rent}
 # Get all apartments in a given city
 # Input is the link to the cities page
 # Returns a list containing links to the apartments
+
+
 def get_listing_links_list():
     main_page = 'https://www.realo.be/en/search/house/for-sale/brugge-8000'
     req = Request(main_page, headers={'User-Agent': 'Mozilla/5.0'})
@@ -30,7 +38,9 @@ def get_listing_links_list():
     main_page_soup = BeautifulSoup(main_page_html, 'html.parser')
 
     results_list = []
-    for each in main_page_soup.findAll('li', attrs={'data-id': 'componentEstateListGridItem'}):
+    for each in main_page_soup.findAll(
+        'li', attrs={
+            'data-id': 'componentEstateListGridItem'}):
         if each.div.div.a:
             results_list.append(each.div.div.a['href'])
 
@@ -41,6 +51,7 @@ def get_listing_links_list():
 # Returns a dict containing the square meters, Rent per month and link to
 # property
 
+
 def get_apartment_info():
     main_page = 'https://www.realo.be/en/sint-gillisdorpstraat-38-8000-brugge/3273431?l=480038643'
     req = Request(main_page, headers={'User-Agent': 'Mozilla/5.0'})
@@ -48,8 +59,10 @@ def get_apartment_info():
 
     main_page_soup = BeautifulSoup(main_page_html, 'html.parser')
 
-    # price = main_page_soup.find('li', attrs={'class':'col module module-price'}).find('div', attrs={'class':'value'}).span.text.strip())
-    habitable_area = main_page_soup.find('div', attrs={'class': 'component-property-features'}).div.table.tbody.findAll('tr')[3].findAll('td')[1].text
-    
-    return {'price':price, 'habitable_area':habitable_area}
+    # price = main_page_soup.find('li', attrs={'class':'col module
+    # module-price'}).find('div', attrs={'class':'value'}).span.text.strip())
+    habitable_area = main_page_soup.find(
+        'div', attrs={
+            'class': 'component-property-features'}).div.table.tbody.findAll('tr')[3].findAll('td')[1].text
 
+    return {'price': price, 'habitable_area': habitable_area}
